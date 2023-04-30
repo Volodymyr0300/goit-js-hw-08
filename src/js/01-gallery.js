eadf08623d6b18a4a4858a390cff8a16c2670d2d
Volodymyr Poliakov <vladimir03002@gmail.com>
2023-04-30 15:46:51 -0500

    first task is done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,62 +1,18 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
-console.log(galleryItems);
 
 const divRef = document.querySelector('.gallery');
 
-function createDivCardMarkup(items) {
-  return items
-    .map(item => {
-      return `
-          <div class="gallery__item">
-              <a class="gallery__link" href="${item.original}">
-                  <img
-                      class="gallery__image"
-                      src="${item.preview}"
-                      data-source="${item.original}"
-                      alt="${item.description}"
-                  />
-              </a>
-          </div>
-          `;
-    })
-    .join('');
-}
+const createCardMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__list"><a class-"gallery__item" href-"${original}">
+    <img class="gallery__image" src="${preview}" alt="${description} data-source='${original}" /></a></li>`;
+  })
+  .join('');
 
-const createGalleryMarcup = createDivCardMarkup(galleryItems);
+divRef.innerHTML = createCardMarkup;
 
-divRef.innerHTML = createGalleryMarcup;
-
-divRef.addEventListener('click', onImgClick, {
-  onClose: () => {
-    document.removeEventListener('keydown', closeModal);
-  },
-});
-
-function onImgClick(e) {
-  e.preventDefault();
-  if (e.target.nodeName !== 'IMG') {
-    return;
-  }
-
-  const instance = basicLightbox.create(
-    `
-    <img src="${e.target.dataset.source}" width="800" height="600">
-`,
-    {
-      onShow: () => document.addEventListener('keydown', onEscPress),
-      onClose: () => document.removeEventListener('keydown', onEscPress),
-    }
-  );
-  instance.show();
-
-  function onEscPress(e) {
-    if (e.code === 'Escape') {
-      instance.close();
-    }
-  }
-}
-
-console.log('hello');
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
