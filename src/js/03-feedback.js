ea74b5c3369b35462cfbcce0500a987855728f82
Volodymyr Poliakov <vladimir03002@gmail.com>
2023-04-30 19:47:18 -0500

    done third task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('form');
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+const localStorageDataOfForm = {};
+
+formEl.addEventListener('input', throttle(writingForLocalStorage, 500));
+formEl.addEventListener('submit', btnSubmitClick);
+
+reloadPageClicking();
+
+function writingForLocalStorage(e) {
+  localStorageDataOfForm[e.target.name] = e.target.value;
+  localStorage.setItem(
+    LOCAL_STORAGE_KEY,
+    JSON.stringify(localStorageDataOfForm)
+  );
+}
+
+function btnSubmitClick(e) {
+  e.preventDefault();
+  formEl.reset();
+  console.log(localStorageDataOfForm);
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
+}
+
+function reloadPageClicking() {
+  const savingParametrsOfForm = JSON.parse(
+    localStorage.getItem(LOCAL_STORAGE_KEY)
+  );
+  if (savingParametrsOfForm) {
+    formEl.email.value = savingParametrsOfForm.email || '';
+    formEl.message.value = savingParametrsOfForm.message || '';
+  }
+}
